831abedd0db20a8dddff40a7c7175eda6740b659
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-05 10:20:06 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const Login = ({ isLoggedIn, handleLogin }) => {
   return (
     <div>
       <p>Login</p>
-      <button onClick={handleLogin}>{isLoggedIn ? 'Logout' : 'Login'}</button>
+      <button onClick={handleLogin}>{isLoggedIn ? 'Log Out' : 'Log In'}</button>
     </div>
   );
 };
@@ -49,8 +49,8 @@ const App = () => {
     <BrowserRouter>
       <p>
         {isLoggedIn
-          ? 'You are logged in. Now you can enter the PlayGround.'
-          : "You are not authenticated please login first."}
+          ? 'Logged in, Now you can enter Playground'
+          : "You are not authenticated, Please login first"}
       </p>
       <nav>
         <ul>
